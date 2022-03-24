1b2d80cc0bca137d4f08ea255b52355558fb027c
Camiloarceg <camiloarceg@gmail.com>
2022-03-24 10:47:49 -0500

    add task 16

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+size_t binary_tree_size(const binary_tree_t *tree);
+
+/**
+ * binary_tree_is_perfect -
+ * @tree: the tree.
+ * Return: 1 is perfect 0 if not.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_size - measures the size of a binary tree.
+ *
+ * @tree: the tree.
+ * Return: the size of the tree.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t lh = 0, rh = 0;
+
+	if (!tree)
+		return (0);
+	lh = binary_tree_size(tree->left);
+	rh = binary_tree_size(tree->right);
+	return (lh + rh + 1);
+}
