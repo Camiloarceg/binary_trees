55adea6876062fc0c6a8613374aac1caf171e9c0
Camiloarceg <camiloarceg@gmail.com>
2022-03-24 10:52:24 -0500

    add task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -3,17 +3,17 @@
 size_t binary_tree_size(const binary_tree_t *tree);
 
 /**
- * binary_tree_is_perfect -
+ * binary_tree_is_perfect -checks if tree is perfect.
  * @tree: the tree.
  * Return: 1 is perfect 0 if not.
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-    if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
-        return (1);
-    return (0);
+	if (!tree)
+		return (0);
+	if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
+		return (1);
+	return (0);
 }
 
 /**
@@ -24,11 +24,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t lh = 0, rh = 0;
+	size_t lh = 0, rh = 0;
 
-    if (!tree)
-        return (0);
-    lh = binary_tree_size(tree->left);
-    rh = binary_tree_size(tree->right);
-    return (lh + rh + 1);
+	if (!tree)
+		return (0);
+	lh = binary_tree_size(tree->left);
+	rh = binary_tree_size(tree->right);
+	return (lh + rh + 1);
 }
